ac6d497d129c033e134b738019d11d168d4dbe2c
SarthakNarayan <sarthak.narayang@gmail.com>
2018-12-21 23:19:37 +0530

    SVM_Regression

--- ML/SVM_Regression.py
+++ ML/SVM_Regression.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection  import train_test_split
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
 
 boston_dataset = load_boston()
 #print(boston.data)
@@ -22,13 +23,24 @@ X_train, X_test, y_train, y_test = train_test_split(originial_X_train ,
 X_train = MinMaxScaler(feature_range=(0, 1)).fit_transform(X_train)
 X_test =  MinMaxScaler(feature_range=(0, 1)).fit_transform(X_test)
 
-svm_reg = LinearSVR(epsilon=0.001 , C=100)
-svm_reg.fit(X_train, y_train)
+regressor_type = 1
+def TypeOfRegressor(choice = 1):
+    if(choice == 1):
+        svm_reg = LinearSVR(epsilon=0.001 , C=100)
+        return svm_reg
+    
+    if(choice == 2):
+        rf_reg = RandomForestRegressor(n_estimators=1000 , min_samples_leaf=10 , n_jobs=1)
+        return rf_reg
+
+# takes time to execute
+regressor = TypeOfRegressor(regressor_type)
+regressor.fit(X_train, y_train)
 
 predictions = []
 for i in X_test:
     i = np.reshape(i , (1,13))
-    predict = svm_reg.predict(i)
+    predict = regressor.predict(i)
     predictions.append(predict)
 
 predictions = np.array(predictions)
@@ -38,4 +50,11 @@ def Tester(test):
     print("test data" , y_test[test])
     print("Predicted data" , predictions[test][0])
 
-Tester(40)
+Tester(15)
+if (regressor_type == 1):
+    print("Linear regressor accuracy" , regressor.score(X_test , y_test))
+if (regressor_type == 2):
+    print("Random forest regressor accuracy" , regressor.score(X_test , y_test))
+
+# ALTHOUGH THE ACCURACY OF Linear regressor IS LESS THAN Random forest regressor 
+# BUT PREDICTION SCORE IS VERY CLOSE TO REAL VALUE
